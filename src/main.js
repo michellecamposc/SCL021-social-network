4957205b379456590d4fea223c00dd3b49a00d4d
Vanecmorales <vanessagm.91@gmail.com>
2022-08-31 17:56:09 -0400

    impresión del login desde el DOM

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 // Este es el punto de entrada de tu aplicacion
 
-
 import { login } from './views/login.js';
 
 const rootContainer = document.getElementById('root');
 rootContainer.appendChild(login());
-
-
 
 // function firstPage() {
 //   loginContainer.innerHTML(login());
