ecdfc281f91c7cc5f0cf65b44c3c617fe42c865c
michellecamposc <stephaniecampoos95@gmail.com>
2022-09-01 17:27:27 -0400

    Eslint actualizado

--- src/main.js
+++ src/main.js
@@ -1,13 +1,6 @@
 // Este es el punto de entrada de tu aplicacion
 
+import { login } from "./views/login.js";
 
-import { login } from './views/login.js';
-
-const rootContainer = document.getElementById('root');
+const rootContainer = document.getElementById("root");
 rootContainer.appendChild(login());
-
-
-
-// function firstPage() {
-//   loginContainer.innerHTML(login());
-// }
--- src/views/login.js
+++ src/views/login.js
@@ -1,65 +1,69 @@
 export function login() {
-  const loginContainer = document.createElement('div');
+  const loginContainer = document.createElement("div");
 
-  const logo = document.createElement('img');
-  logo.setAttribute('src', '../images/logoazul.png');
+  const logo = document.createElement("img");
+  logo.setAttribute("src", "../images/logoazul.png");
+  logo.classList.add("logo");
   loginContainer.appendChild(logo);
 
-  const logoTitle = document.createElement('h1');
-  logoTitle.textContent = 'Go Wild Nature';
-  logoTitle.classList.add('logoTitle');
+  const logoTitle = document.createElement("h1");
+  logoTitle.textContent = "Go Wild Nature";
+  logoTitle.classList.add("logoTitle");
   loginContainer.appendChild(logoTitle);
 
-  const form = document.createElement('form');
-  form.classList.add('form');
+  const form = document.createElement("form");
+  form.classList.add("form");
   loginContainer.appendChild(form);
 
-  const inputMail = document.createElement('input');
-  inputMail.classList.add('inputLogin');
-  inputMail.setAttribute('type', 'email');
-  inputMail.setAttribute('placeholder', 'Email');
+  const inputMail = document.createElement("input");
+  inputMail.classList.add("inputLogin");
+  inputMail.setAttribute("type", "email");
+  inputMail.setAttribute("placeholder", "Email");
   form.appendChild(inputMail);
 
-  const inputPassword = document.createElement('input');
-  inputPassword.classList.add('inputPassword');
-  inputPassword.setAttribute('type', 'password');
+  const inputPassword = document.createElement("input");
+  inputPassword.classList.add("inputLogin");
+  inputPassword.setAttribute("type", "password");
+  inputPassword.setAttribute("placeholder", "Password");
   form.appendChild(inputPassword);
 
-  const loginSubmit = document.createElement('input');
-  loginSubmit.classList.add('loginSubmit');
-  loginSubmit.setAttribute('type', 'submit');
-  loginSubmit.setAttribute('name', 'Login');
+  const loginSubmit = document.createElement("input");
+  loginSubmit.classList.add("loginSubmit");
+  loginSubmit.setAttribute("type", "submit");
+  loginSubmit.setAttribute("name", "Login");
   form.appendChild(loginSubmit);
 
-  const signupOption = document.createElement('p');
-  signupOption.textContent = 'Or sign up with';
-  signupOption.classList.add('signupOption');
+  const signupOption = document.createElement("p");
+  signupOption.textContent = "Or sign up with";
+  signupOption.classList.add("signupOption");
   loginContainer.appendChild(signupOption);
 
   // Contenedor de botones del login
 
-  const loginButtonsContainer = document.createElement('div');
-  loginButtonsContainer.classList.add('loginButtonsContainer');
+  //Contenedor con todos los botones
+  const loginButtonsContainer = document.createElement("div");
+  loginButtonsContainer.classList.add("loginButtonsContainer");
   loginContainer.appendChild(loginButtonsContainer);
 
-  const loginGoogle = document.createElement('button');
-  // Imagen del boton
-  loginGoogle.classList.add('loginGoogle');
+  //Botón de Google
+  const loginGoogle = document.createElement("button");
+  loginGoogle.classList.add("loginGoogle");
   loginButtonsContainer.appendChild(loginGoogle);
 
-  const loginFacebook = document.createElement('button');
-  // Imagen del boton
-  loginFacebook.classList.add('loginFacebook');
+  //Botón de Facebook
+  const loginFacebook = document.createElement("button");
+  loginFacebook.classList.add("loginFacebook");
   loginButtonsContainer.appendChild(loginFacebook);
 
-  const loginApple = document.createElement('button');
-  // Imagen del boton
-  loginApple.classList.add('loginApple');
+  //Botón de Apple
+  const loginApple = document.createElement("button");
+  loginApple.classList.add("loginApple");
   loginButtonsContainer.appendChild(loginApple);
 
-  const forgetPassword = document.createElement('p');
-  forgetPassword.textContent = 'Forget Password';
-  forgetPassword.classList.add('forgetPassword');
+  //Olvidaste contraseña
+  const forgetPassword = document.createElement("p");
+  forgetPassword.textContent = "Forget Password";
+  forgetPassword.classList.add("forgetPassword");
   loginContainer.appendChild(forgetPassword);
 
   return loginContainer;
